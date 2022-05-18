d4e30d80fa8a05b84b67460e2ee519ef9a13e709
ArcherGu <649601376@qq.com>
2022-05-18 15:41:47 +0800

    chore: update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -7,38 +7,35 @@ import { writeFileSync } from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: join(__dirname, 'src/render'),
-    plugins: [
-        vue(),
-        VitePluginElectronBuilder({
-            root: process.cwd(),
-            tsconfig: "./tsconfig.main.json",
-            electronBuilderConfig: "./electron-builder.config.js",
-            external: ['@nestjs'],
-            afterEsbuildBuild: async () => {
-                await compileFile({
-                    filename: './dist/main/index.js',
-                    output: './dist/main/main.jsc',
-                    electron: true,
-                });
+  root: join(__dirname, 'src/render'),
+  plugins: [
+    vue(),
+    VitePluginElectronBuilder({
+      root: process.cwd(),
+      tsconfig: "./tsconfig.main.json",
+      electronBuilderConfig: "./electron-builder.config.js",
+      external: ['@nestjs'],
+      afterEsbuildBuild: async () => {
+        await compileFile({
+          filename: './dist/main/index.js',
+          output: './dist/main/main.jsc',
+          electron: true,
+        });
 
-                writeFileSync('./dist/main/index.js', "require('bytenode');require('./main.jsc')");
-            },
-        })
-    ],
-    resolve: {
-        alias: {
-            '@render': join(__dirname, 'src/render'),
-            '@main': join(__dirname, 'src/main'),
-            '@common': join(__dirname, 'src/common'),
-        },
+        writeFileSync('./dist/main/index.js', "require('bytenode');require('./main.jsc')");
+      },
+    })
+  ],
+  resolve: {
+    alias: {
+      '@render': join(__dirname, 'src/render'),
+      '@main': join(__dirname, 'src/main'),
+      '@common': join(__dirname, 'src/common'),
     },
-    base: './',
-    build: {
-        outDir: join(__dirname, 'dist/render'),
-        emptyOutDir: true
-    },
-    server: {
-        port: +process.env.PORT,
-    }
+  },
+  base: './',
+  build: {
+    outDir: join(__dirname, 'dist/render'),
+    emptyOutDir: true
+  }
 });
