af6c2c521d4777e98051e3bdda94aa1ceda40886
ArcherGu <649601376@qq.com>
2022-07-27 13:59:41 +0800

    chore: remove unnecessary code

--- src/common/types.ts
+++
@@ -1,8 +0,0 @@
-export type Nullable<T> = T | null
-
-export type Voidable<T> = T | null | undefined
-
-export interface IpcResponse<T> {
-  data?: T
-  error?: any
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { join } from 'path'
-import { writeFileSync } from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { compileFile } from 'bytenode'
 import { VitePluginDoubleshot } from 'vite-plugin-doubleshot'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
   root: join(__dirname, 'src/render'),
   plugins: [
     vue(),
@@ -18,16 +13,7 @@ export default defineConfig({
       main: 'dist/main/index.js',
       entry: 'src/main/index.ts',
       outDir: 'dist/main',
-      external: ['electron', '@nestjs'],
-      afterBuild: async () => {
-        await compileFile({
-          filename: './dist/main/index.js',
-          output: './dist/main/main.jsc',
-          electron: true,
-        })
-
-        writeFileSync('./dist/main/index.js', 'require(\'bytenode\');require(\'./main.jsc\')')
-      },
+      external: ['electron'],
       electron: {
         build: {
           config: './electron-builder.config.js',
@@ -43,7 +29,6 @@ export default defineConfig({
     alias: {
       '@render': join(__dirname, 'src/render'),
       '@main': join(__dirname, 'src/main'),
-      '@common': join(__dirname, 'src/common'),
     },
   },
   base: './',
