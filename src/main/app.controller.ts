e685b2d57f60172debca120c4c46936857f466af
ArcherGu <649601376@qq.com>
2022-09-07 08:36:15 +0800

    chore: use @Window

--- src/main/app.controller.ts
+++ src/main/app.controller.ts
@@ -1,18 +1,19 @@
 import { Controller } from '@nestjs/common'
-import { ElectronService, IpcHandle } from '@doubleshot/nest-electron'
+import { IpcHandle, Window } from '@doubleshot/nest-electron'
 
+import { BrowserWindow } from 'electron'
 import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private readonly electronService: ElectronService,
+    @Window() private readonly mainWin: BrowserWindow,
   ) { }
 
   @IpcHandle('msg')
   public async handleSendMsg(msg: string): Promise<string> {
-    const { webContents } = this.electronService.getWindow()
+    const { webContents } = this.mainWin
     webContents.send('reply-msg', 'this is msg from webContents.send')
     return `The main process received your message: ${msg} at time: ${this.appService.getTime()}`
   }
