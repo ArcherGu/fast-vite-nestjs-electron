98b1e1c51deccbd6c50b6a444b96dc919e9c72a0
archergu <649601376@qq.com>
2023-02-22 10:43:58 +0800

    chore: update @doubleshot/nest-electron to 0.2.0

--- src/main/app.controller.ts
+++ src/main/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common'
 import { IpcHandle, Window } from '@doubleshot/nest-electron'
-
+import { Payload } from '@nestjs/microservices'
+import { type Observable, of } from 'rxjs'
 import { BrowserWindow } from 'electron'
 import { AppService } from './app.service'
 
@@ -12,9 +13,9 @@ export class AppController {
   ) { }
 
   @IpcHandle('msg')
-  public async handleSendMsg(msg: string): Promise<string> {
+  public handleSendMsg(@Payload() msg: string): Observable<string> {
     const { webContents } = this.mainWin
     webContents.send('reply-msg', 'this is msg from webContents.send')
-    return `The main process received your message: ${msg} at time: ${this.appService.getTime()}`
+    return of(`The main process received your message: ${msg} at time: ${this.appService.getTime()}`)
   }
 }
