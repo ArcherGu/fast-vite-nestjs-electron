ea58f06ea92d01b913da980b333c42e204051ac0
archergu <649601376@qq.com>
2026-02-28 15:08:01 +0800

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,12 @@ import lightwing from '@lightwing/eslint-config'
 
 export default lightwing(
   {
-    ignores: [
-      'dist',
-      'node_modules',
-      '*.svelte',
-      '*.snap',
-      '*.d.ts',
-      'coverage',
-      'js_test',
-      'local-data',
-    ],
+    ignores: ['dist', 'node_modules', '*.svelte', '*.snap', '*.d.ts', 'coverage', 'js_test', 'local-data'],
+  },
+  {
+    files: ['README.md'],
+    rules: {
+      'markdown/fenced-code-language': 'off',
+    },
   },
 )
